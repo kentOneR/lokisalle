149164a1eb244cdaac95a35734e115ec5822db90
kentOneR <blot.quentin@gmail.com>
2018-06-08 17:01:50 +0200

    Admin :  create page review

--- admin.php
+++ admin.php
@@ -10,11 +10,13 @@ if(adminConnect()) :
         <li><a href="admin.php?action=add-member">Ajouter un membre</a></li>
         <li><a href="admin.php?action=show-room">Liste des salles</a></li>
         <li><a href="admin.php?action=add-room">Ajouter une salle</a></li>
+        <li><a href="admin.php?action=show-review">Liste des avis</a></li>
     </ul>
 </nav>
 
 <?php require_once('template/manage-member.php'); ?>
 <?php require_once('template/manage-room.php'); ?>
+<?php require_once('template/manage-review.php'); ?>
 
 <?php else:
 
---
+++ template/manage-review.php
@@ -0,0 +1,6 @@
+<?php 
+if(isset($_GET['action']) && $_GET['action'] == "show-review"):
+
+echo 'ici la liste de avis';
+
+endif; ?>
