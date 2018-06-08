ade2ba1f05a3a66a44e5ad5762b821805cf2cecd
kentOneR <blot.quentin@gmail.com>
2018-06-08 11:29:57 +0200

    Admin : update or add room

---
+++ inc/function.room.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once('init.inc.php');
+
+if($_POST && isset($_SESSION['token']) && isset($token) && !empty($_SESSION['token']) && !empty($token)){
+    $photoDB = '';
+    if ($_SESSION['token'] == $token) {
+        if(isset($_POST) && isset($_POST['photo-old'])){
+            $photoDB = $_POST['photo-old'];
+        }
+        if(!empty($_FILES['photo']['name'])) {
+            $error = false;
+            $tempFile = $_FILES['photo']['tmp_name'];
+            $actualSize = $_FILES['photo']['size'];
+
+            $newName = bin2hex(mcrypt_create_iv(8, MCRYPT_DEV_URANDOM));
+            $legalExtensions = array("JPG", "PNG", "jpg", "png");
+            $legalSize = "1000000"; // 1000000 Octets = 1 MO
+
+            $extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
+            $photoName = $_POST['reference'].'_'.$newName.'.'.$extension;
+            $photoFolder = $_SERVER['DOCUMENT_ROOT'].'/back_php_project\/photo\/'.$photoName;
+            $photoDB = $photoName;
+
+            // On s'assure que le fichier n'est pas vide
+            if ($actualSize == 0) {
+                $error = true;
+            }
+
+            // On vérifie qu'un fichier portant le même nom n'est pas présent sur le serveur
+            if (file_exists($photoFolder)) {
+                $error = true;
+            }
+
+            // On effectue nos vérifications réglementaires
+            if (!$error) {
+                if ($actualSize < $legalSize) {
+                    if (in_array($extension, $legalExtensions)) {
+                        copy($tempFile, $photoFolder);
+                    }
+                }
+            } else {
+                @unlink($photoFolder);
+                echo "Une erreur s'est produite";
+            }
+        }
+        foreach ($_POST as $key => $value) {
+            if($key != 'category') {
+                $_POST[$key] = htmlentities(addslashes($value));
+            }
+        }
+        if(isset($_POST['id-room'])){
+            $idRoom = $_POST['id-room'];
+            $roomReq = $pdo->prepare("UPDATE salle SET 
+                titre = '$_POST[title]',
+                description = '$_POST[description]',
+                photo = '$photoDB',
+                pays = '$_POST[country]',
+                ville = '$_POST[city]',
+                adresse = '$_POST[address]',
+                cp = '$_POST[cp]',
+                capacite = '$_POST[capacity]',
+                categorie = '$_POST[category]'
+            WHERE id_salle = ? ");
+            $roomReq->execute(array($idRoom));
+        } else {
+            $roomReq = $pdo->prepare("INSERT INTO salle( titre, description, photo, pays, ville, adresse, cp, capacity, categorie) VALUES
+            (:title, :description, :photo, :country, :city, :address, :cp, :capacity, :category)");
+            $roomReq->execute(array(
+                "title" => $_POST['title'], 
+                "description" => $_POST['description'],
+                "photo" => $photoDB,
+                "country" => $_POST['country'],
+                "city" => $_POST['city'],
+                "address" => $_POST['address'],
+                "cp" => $_POST['cp'],
+                "capacity" => $_POST['capacity'],
+                "category" => $_POST['category']
+            ));
+        }
+        header('location:../admin?action=show-room');
+    }
+} else {
+
+    // Les token ne correspondent pas
+    echo "Erreur de vérification";
+}
+
+
+?>
--- template/manage-room.php
+++ template/manage-room.php
@@ -5,7 +5,7 @@
     $columnRoomList = $r->columnCount();
 
     ?>
-    <h2>Liste des membres</h2>
+    <h2>Liste des salles</h2>
     <table border="1" cellpadding="5">
         <tr>
         <?php for ($i=0; $i < $columnRoomList  ; $i++) { 
@@ -29,5 +29,65 @@
             </tr>
         <?php } ?>
     </table>
+
+<?php endif; ?>
+
+<?php 
+
+if(isset($_GET['action']) && ($_GET['action'] == "add-room" || $_GET['action'] == "edit-room")) :
+
+    // récupération des membres
+    if(isset($_GET['id_room'])) {
+        $r = $pdo->prepare("SELECT * FROM salle WHERE id_salle = ? ");
+        $r->execute(array($_GET['id_room']));
+        $room = $r->fetch(PDO::FETCH_ASSOC);
+    }
+
+    $id_room = (isset($room['id_salle'])) ? $room['id_salle'] : '';
+    $title = (isset($room['titre'])) ? $room['titre'] : '';
+    $description = (isset($room['description'])) ? $room['description'] : '';
+    $photo = (isset($room['photo'])) ? $room['photo'] : '';
+    $country = (isset($room['pays'])) ? $room['pays'] : '';
+    $city = (isset($room['ville'])) ? $room['ville'] : '';
+    $address = (isset($room['adresse'])) ? $room['adresse'] : '';
+    $cp = (isset($room['cp'])) ? $room['cp'] : '';
+    $capacity = (isset($room['capacite'])) ? $room['capacite'] : '';
+    $category = (isset($room['categorie'])) ? $room['categorie'] : '';
+
+
+?>
+
+<h2>Gestion des salles</h2>
+
+<form class="col-sm-6" action="inc/function.room.php" method="post">
+    <input type="hidden" class="form-control" name="id-room" id="id-room" value="<?= $id_room ?>">
+    <label for="title">Titre</label><br>
+    <input type="text" class="form-control" name="title" id="title" value="<?= $title ?>">
+    <label for="description">Description</label><br>
+    <input type="text" class="form-control" name="description" id="description" value="<?= $description ?>">
+    <?php if($photo != '') { ?>
+        <img src="img/room/<?= $photo ?>" alt="" width="80" height="80"><br>
+        <input type="hidden" name="photo-old" id="photo-old" value="<?= $photo ?>">
+    <?php } ?>
+    <label for="photo">Photo</label>
+    <input type="file" class="form-control" name="photo" id="photo" value="<?= $photo ?>">
+    <label for="description">Pays</label><br>
+    <input type="text" class="form-control" name="country" id="country" value="<?= $country ?>">
+    <label for="city">Ville</label><br>
+    <input type="text" class="form-control" name="city" id="city" value="<?= $city ?>">
+    <label for="address">Adresse</label><br>
+    <input type="text" class="form-control" name="address" id="address" value="<?= $address ?>">
+    <label for="cp">CP</label><br>
+    <input type="text" class="form-control" name="cp" id="cp" value="<?= $cp ?>">
+    <label for="capacity">Capacité</label><br>
+    <input type="text" class="form-control" name="capacity" id="capacity" value="<?= $capacity ?>">
+    <label for="category">Categorie</label><br>
+    <select class="form-control" name="category" id="category" value="<?= $category ?>">
+        <option value="réunion" class="form-control">réunion</option>
+        <option value="séminaire" class="form-control">séminaire</option>
+        <option value="formation" class="form-control">formation</option>
+    <select>
+    <input type="submit" class="btn btn-default" name="managemember" id="managemember" value="Valider">
+</form>
 
 <?php endif; ?>
