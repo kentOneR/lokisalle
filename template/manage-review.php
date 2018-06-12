bf297b420da50ad901c918f024db080565d3b6ae
kentOneR <blot.quentin@gmail.com>
2018-06-12 12:34:05 +0200

    admin: delete review

--- template/manage-review.php
+++ template/manage-review.php
@@ -1,44 +1,50 @@
 <?php 
-if(isset($_GET['action']) && $_GET['action'] == "show-review"):
+if(isset($_GET['action'])):
 
-    $r = $pdo->prepare("SELECT * FROM avis");
-    $r->execute();
-    $columnReview = $r->columnCount();
-    $reviewReq = $r->fetchAll(PDO::FETCH_ASSOC);
+    if($_GET['action'] == "show-review" || $_GET['action'] == "delete-review"){
+        $r = $pdo->prepare("SELECT * FROM avis");
+        $r->execute();
+        $columnReview = $r->columnCount();
+        $reviewReq = $r->fetchAll(PDO::FETCH_ASSOC);
 
-?>
-
-
-    <h2>Liste des avis</h2>
-    <table border="1" cellpadding="5">
-        <tr>
-        <?php for ($i=0; $i < $columnReview  ; $i++) { 
-            $column = $r->getColumnMeta($i); ?>
-            <th><?=$column['name']?></th>
-        <?php } ?>
-            <th>action</th>
-        </tr>
-        <?php foreach ($reviewReq as $key => $review) { ?>
+        ?>
+        <h2>Liste des avis</h2>
+        <table border="1" cellpadding="5">
             <tr>
-                <?php foreach ($review as $key => $value) { ?>
-                        <td> <?= $value ?></td>
-                <?php } ?>
-                <td>
-                    <a href="?action=edit-review&id_review=<?= $review['id_avis'] ?>"><i class="far fa-edit"></i></a>
-                    <a href="?action=delete-review&id_review=<?= $review['id_avis'] ?>" onClick="confirm(\'En êtes vous sur?\')"><i class="far fa-trash-alt"></i></a>
-                </td>
+            <?php for ($i=0; $i < $columnReview  ; $i++) { 
+                $column = $r->getColumnMeta($i); ?>
+                <th><?=$column['name']?></th>
+            <?php } ?>
+                <th>action</th>
             </tr>
-        <?php } ?>
-    </table>
+            <?php foreach ($reviewReq as $key => $review) { ?>
+                <tr>
+                    <?php foreach ($review as $key => $value) { ?>
+                            <td> <?= $value ?></td>
+                    <?php } ?>
+                    <td>
+                        <a href="?action=edit-review&id_review=<?= $review['id_avis'] ?>"><i class="far fa-edit"></i></a>
+                        <a href="?action=delete-review&id_review=<?= $review['id_avis'] ?>" onClick="confirm(\'En êtes vous sur?\')"><i class="far fa-trash-alt"></i></a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+
+    <?php } elseif($_GET['action'] == "delete-review"){
+        $deleteReviewReq = $pdo->prepare("DELETE FROM avis WHERE id_avis = ?");
+        $deleteReviewReq->execute(array($_GET['id_review']));
+        
+    }
+?>
 
 <?php elseif(isset($_GET['action']) && $_GET['action'] == "edit-review"): 
 
 // Récupéation de l'avis
-if(isset($_GET['id_review'])) {
-    $r = $pdo->prepare("SELECT * FROM avis WHERE id_avis = ? ");
-    $r->execute(array($_GET['id_review']));
-    $thisReviewReq = $r->fetch(PDO::FETCH_ASSOC);
-}
+    if(isset($_GET['id_review'])) {
+        $r = $pdo->prepare("SELECT * FROM avis WHERE id_avis = ? ");
+        $r->execute(array($_GET['id_review']));
+        $thisReviewReq = $r->fetch(PDO::FETCH_ASSOC);
+    }
 
 $idReview = (isset($thisReviewReq['id_avis'])) ? $thisReviewReq['id_avis'] : '';
 $idMember = (isset($thisReviewReq['id_membre'])) ? $thisReviewReq['id_membre'] : '';
