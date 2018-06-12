02e57d2228ef5b8bfb5c3cb291216e63c1aee43c
kentOneR <blot.quentin@gmail.com>
2018-06-12 12:56:04 +0200

    Admin : add stars for review

--- template/manage-review.php
+++ template/manage-review.php
@@ -19,9 +19,17 @@ if(isset($_GET['action'])):
             </tr>
             <?php foreach ($reviewReq as $key => $review) { ?>
                 <tr>
-                    <?php foreach ($review as $key => $value) { ?>
-                            <td> <?= $value ?></td>
-                    <?php } ?>
+                    <?php foreach ($review as $key => $value) { 
+                        if($key != 'note'){ ?>
+                            <td><?= $value ?></td>
+                        <?php }else{ ?>
+                            <td>
+                                <?php for ($i=0; $i < $value; $i++) { ?>
+                                    <i class="fas fa-star"></i>
+                                <?php } ?>
+                            </td>
+                        <?php }
+                        } ?>
                     <td>
                         <a href="?action=edit-review&id_review=<?= $review['id_avis'] ?>"><i class="far fa-edit"></i></a>
                         <a href="?action=delete-review&id_review=<?= $review['id_avis'] ?>" onClick="confirm(\'En êtes vous sur?\')"><i class="far fa-trash-alt"></i></a>
@@ -35,24 +43,21 @@ if(isset($_GET['action'])):
         $deleteReviewReq->execute(array($_GET['id_review']));
         
     }
-?>
+    elseif(isset($_GET['action']) && $_GET['action'] == "edit-review"){
 
-<?php elseif(isset($_GET['action']) && $_GET['action'] == "edit-review"): 
+        // Récupéation de l'avis
+        if(isset($_GET['id_review'])) {
+            $r = $pdo->prepare("SELECT * FROM avis WHERE id_avis = ? ");
+            $r->execute(array($_GET['id_review']));
+            $thisReviewReq = $r->fetch(PDO::FETCH_ASSOC);
+        }
 
-// Récupéation de l'avis
-    if(isset($_GET['id_review'])) {
-        $r = $pdo->prepare("SELECT * FROM avis WHERE id_avis = ? ");
-        $r->execute(array($_GET['id_review']));
-        $thisReviewReq = $r->fetch(PDO::FETCH_ASSOC);
-    }
-
-$idReview = (isset($thisReviewReq['id_avis'])) ? $thisReviewReq['id_avis'] : '';
-$idMember = (isset($thisReviewReq['id_membre'])) ? $thisReviewReq['id_membre'] : '';
-$idRoom = (isset($thisReviewReq['id_salle'])) ? $thisReviewReq['id_salle'] : '';
-$reviewTxt = (isset($thisReviewReq['commentaire'])) ? $thisReviewReq['commentaire'] : '';
-$reviewNote = (isset($thisReviewReq['note'])) ? $thisReviewReq['note'] : '';
-
-?>
+    $idReview = (isset($thisReviewReq['id_avis'])) ? $thisReviewReq['id_avis'] : '';
+    $idMember = (isset($thisReviewReq['id_membre'])) ? $thisReviewReq['id_membre'] : '';
+    $idRoom = (isset($thisReviewReq['id_salle'])) ? $thisReviewReq['id_salle'] : '';
+    $reviewTxt = (isset($thisReviewReq['commentaire'])) ? $thisReviewReq['commentaire'] : '';
+    $reviewNote = (isset($thisReviewReq['note'])) ? $thisReviewReq['note'] : '';
+    ?>
 
     <form class="col-sm-6" action="inc/function.review.php" method="post">
         <input type="hidden" class="form-control" name="id-review" value="<?= $idReview ?>">
@@ -74,7 +79,7 @@ $reviewNote = (isset($thisReviewReq['note'])) ? $thisReviewReq['note'] : '';
         <input type="submit" class="btn btn-default" name="edit-review" value="Valider">
     </form>
 
-
+    <?php } ?>
 
 
 
