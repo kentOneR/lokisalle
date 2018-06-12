893bc957926410187aa19457320c2f32f0dd380a
kentOneR <blot.quentin@gmail.com>
2018-06-12 10:59:24 +0200

    Review : check if it's number

--- inc/function.review.php
+++ inc/function.review.php
@@ -4,11 +4,13 @@ require_once('init.inc.php');
 
 if(isset($_POST)) {
     $idMembre = $_SESSION['membre']['id_membre'];
+    $noteReview = $_POST['note'];
+    ($noteReview > 5) ? $noteReview = 5 : (($noteReview < 0) ? $noteReview = 0 : $noteReview = $noteReview);
     $addReviewReq = $pdo->prepare("INSERT INTO avis (id_membre, id_salle, note, commentaire) VALUES (:id_membre, :id_salle, :note, :commentaire)");
     $addReviewReq->execute(array(
         'id_membre' => $idMembre,
         'id_salle' => $_POST['id-room'],
-        'note' => $_POST['note'],
+        'note' => $noteReview,
         'commentaire' => $_POST['review']
     ));
 
--- template/add-review.php
+++ template/add-review.php
@@ -1,6 +1,6 @@
 <?php if(isset($_GET['action']) && $_GET['action'] == 'add-review' && isset($_GET['id-room'])) { ?>
 
-    <form class="col-sm-6" action="function.review.php" method="post">
+    <form class="col-sm-6" action="inc/function.review.php" method="post">
         <input class="form-control" type="hidden" name="id-room" value="<?= $_GET['id-room'] ?>">
         <label for="note">Note:</label>
         <select class="form-control" name="note">
@@ -8,7 +8,7 @@
             <option value="1">1</option>
             <option value="2">5</option>
             <option value="3">4</option>
-            <option value="5">5</option>
+            <option value="5" selected>5</option>
         </select>
         <label for="review">Commentaire:</label>
         <textarea class="form-control" name="review" cols="10" rows="5"></textarea><br>
