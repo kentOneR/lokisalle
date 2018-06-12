c003200555f614aad65f5d1615caec156eb34ce5
kentOneR <blot.quentin@gmail.com>
2018-06-12 16:38:37 +0200

    Stat : member nb_order

--- template/admin/manage-stat.php
+++ template/admin/manage-stat.php
@@ -15,9 +15,16 @@ $topOrderReq = $pdo->prepare("SELECT c.id_salle, s.titre, COUNT(c.id_commande) a
 $topOrderReq->execute();
 $topOrderReq = $topOrderReq->fetchAll(PDO::FETCH_ASSOC);
 
+$topMemberOrderReq = $pdo->prepare("SELECT c.id_membre, m.pseudo, COUNT(c.id_commande) as nb_order FROM commande c, membre m WHERE c.id_membre=m.id_membre GROUP BY id_membre ORDER BY nb_order DESC LIMIT 5");
+$topMemberOrderReq->execute();
+$topMemberOrderReq = $topMemberOrderReq->fetchAll(PDO::FETCH_ASSOC);
+
+$topMemberBuyReq = $pdo->prepare("SELECT c.id_membre, m.pseudo, COUNT(c.id_commande) as nb_order FROM commande c, membre m WHERE c.id_membre=m.id_membre GROUP BY id_membre ORDER BY nb_order DESC LIMIT 5");
+$topMemberBuyReq->execute();
+$topMemberBuyReq = $topMemberBuyReq->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-
+<h5>Top 5 des salles les mieux notées</h5>
 <table border="1" cellpadding="5">
     <tr>
         <th>id_salle</th>
@@ -32,16 +39,32 @@ $topOrderReq = $topOrderReq->fetchAll(PDO::FETCH_ASSOC);
     </tr>
     <?php } ?>
 </table>
+<h5>Top 5 des salles les plus commandées</h5>
 <table border="1" cellpadding="5">
     <tr>
         <th>id_salle</th>
         <th>titre</th>
-        <th>Nb Commandes</th>
+        <th>Nb Cde</th>
     </tr>
     <?php foreach ($topOrderReq as $key => $value) { ?>
     <tr>
         <th><?= $value['id_salle'] ?></th>
         <th><?= $value['titre'] ?></th>
+        <th><?= $value['nb_order'] ?></th>
+    </tr>
+    <?php } ?>
+</table>
+<h5>Top 5 des membre qui ont passé le plus de commande</h5>
+<table border="1" cellpadding="5">
+    <tr>
+        <th>id_membre</th>
+        <th>pseudo</th>
+        <th>Nb Cde</th>
+    </tr>
+    <?php foreach ($topMemberOrderReq as $key => $value) { ?>
+    <tr>
+        <th><?= $value['id_membre'] ?></th>
+        <th><?= $value['pseudo'] ?></th>
         <th><?= $value['nb_order'] ?></th>
     </tr>
     <?php } ?>
